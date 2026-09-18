355fd68f17382c992bec22b17e89ff17dec42dab
Imtiaz Ali <imtiazali17314@gmail.com>
2026-09-18 13:11:08 +0500

    feat: enhance Home screen with user account management and logout functionality

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -1,7 +1,6 @@
 import Home from '../screens/Home';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
 
 type AppStackParamList = {
   Home: undefined;
@@ -11,7 +10,6 @@ const Stack = createNativeStackNavigator<AppStackParamList>();
 
 export const AppStack = () => {
   return (
-    <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
           headerTitleAlign: 'center',
@@ -20,7 +18,6 @@ export const AppStack = () => {
       >
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
-    </NavigationContainer>
   );
 };
 
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,48 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import AppwriteContext from '../appwrite/AppwriteContext';
+import { Snackbar } from 'react-native-snackbar';
+
+type User = {
+  name: string;
+  email: string;
+};
 
 const Home = () => {
+  const [user, setUser] = useState<User>();
+
+  const { appwrite, setIsLoggedIn } = useContext(AppwriteContext);
+
+  const handleLogout = () => {
+    appwrite.logoutAccount().then(() => {
+      setIsLoggedIn(false);
+      Snackbar.show({
+        text: 'Logged out successfully',
+        duration: Snackbar.LENGTH_SHORT,
+      });
+    });
+  };
+
+  useEffect(() => {
+    appwrite
+      .getAccountDetails()
+      .then(response => {
+        if (response) {
+          setUser({
+            name: response.name,
+            email: response.email,
+          } as User);
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching account details:', error);
+        Snackbar.show({
+          text: 'Error fetching account details',
+          duration: Snackbar.LENGTH_SHORT,
+        });
+      });
+  }, []);
+
   return (
     <View>
       <Text>Home</Text>
