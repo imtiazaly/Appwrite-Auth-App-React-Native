2736543f1dfec75174e7141e8c58a782d91b4954
Imtiaz Ali <imtiazali17314@gmail.com>
2026-09-18 13:25:03 +0500

    feat: enhance Home screen with user details display and logout functionality

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import React, { useContext, useEffect, useState } from 'react';
 import AppwriteContext from '../appwrite/AppwriteContext';
 import { Snackbar } from 'react-native-snackbar';
+import { FAB, Icon } from '@rneui/themed';
+import Ionicons from '@react-native-vector-icons/ionicons';
 
 type User = {
   name: string;
@@ -44,12 +46,62 @@ const Home = () => {
   }, []);
 
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.welcomeContainer}>
+        <Image
+          source={{
+            uri: 'https://appwrite.io/images-ee/blog/og-private-beta.png',
+            width: 400,
+            height: 300,
+            cache: 'default',
+          }}
+          resizeMode="contain"
+        />
+        <Text style={styles.message}>
+          Build Fast. Scale Big. All in One Place.
+        </Text>
+        {user && (
+          <View style={styles.userContainer}>
+            <Text style={styles.userDetails}>Name: {user.name}</Text>
+            <Text style={styles.userDetails}>Email: {user.email}</Text>
+          </View>
+        )}
+      </View>
+      <FAB
+        placement="right"
+        color="#f02e65"
+        size="large"
+        title="Logout"
+        icon={<Ionicons name="log-out-outline" size={24} color="white" />}
+        onPress={handleLogout}
+      />
+    </SafeAreaView>
   );
 };
 
-export default Home;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0B0D32',
+  },
+  welcomeContainer: {
+    padding: 12,
 
-const styles = StyleSheet.create({});
+    flex: 1,
+    alignItems: 'center',
+  },
+  message: {
+    fontSize: 26,
+    fontWeight: '500',
+    color: '#FFFFFF',
+  },
+  userContainer: {
+    marginTop: 24,
+  },
+  userDetails: {
+    fontSize: 20,
+    color: '#FFFFFF',
+  },
+});
+
+export default Home;
